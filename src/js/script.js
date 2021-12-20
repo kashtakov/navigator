7dde2e9d5f433fe5e30ae25cad90ce4515721bbc
Serhii Kashtakov <kashtakovs@gmail.com>
2021-12-20 18:25:14 +0200

    20-12

---
+++ src/js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.choose__carousel').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        prevArrow:'<button type="button" class="slick-prev"><img src="../icons/Arrow_prev.svg"></button>',
+        nextArrow:'<button type="button" class="slick-next"><img src="../icons/Arrow_next.svg"></button>',
+     
+    });
+  });
